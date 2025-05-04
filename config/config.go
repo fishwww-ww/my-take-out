9646e906e4d7bd0b69232752184d91280983ae8d
fishwww-ww <2470335462@qq.com>
2025-05-04 23:20:58 +0800

    feat: config文件夹配置

--- config/config.go
+++ config/config.go
@@ -1,22 +1,68 @@
 package config
 
+import (
+	"fmt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+var envPtr = pflag.String("env", "dev", "Environment: dev or prod")
+
+func InitLoadConfig() *AllConfig {
+	pflag.Parse()
+
+	config := viper.New()
+	// 设置读取路径
+	config.AddConfigPath("./config")
+	// 设置读取文件名字
+	config.SetConfigName(fmt.Sprintf("application-%s", *envPtr))
+	// 设置读取文件类型
+	config.SetConfigType("yaml")
+	// 读取文件载体
+	var configData *AllConfig
+	// 读取配置文件
+	err := config.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Use Viper ReadInConfig Fatal error config err:%s \n", err))
+	}
+	// 查找对应配置文件
+	err = config.Unmarshal(&configData)
+	if err != nil {
+		panic(fmt.Errorf("read config file to struct err: %s\n", err))
+	}
+	// 打印配置文件信息
+	fmt.Printf("配置文件信息：%+v", configData)
+	return configData
+}
+
 type AllConfig struct {
 	Server     Server
 	DataSource DataSource
+	Jwt        Jwt
 }
 
 type Server struct {
 	Port  string
 	Level string
 }
-
 type DataSource struct {
-	Host     string
-	Port     string
-	Username string
-	Password string
-	DBName   string
-	Config   string
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	DBName   string `yaml:"dbname"` // 用db_name会报错,很奇怪
+	Config   string `yaml:"config"`
+}
+
+type Jwt struct {
+	Admin JwtOption
+	User  JwtOption
+}
+
+type JwtOption struct {
+	Secret string
+	TTL    string
+	Name   string
 }
 
 func (d *DataSource) Dsn() string {
